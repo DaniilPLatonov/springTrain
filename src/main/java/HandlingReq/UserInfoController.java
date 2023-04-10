7f89a73f3cfb37a7d8bfc2015af6f9fb42fe8280
DaniilPlatonov <d_platonov@internet.ru>
2023-04-10 21:59:04 +0300

    Initial Commit

---
+++ src/main/java/HandlingReq/UserInfoController.java
@@ -0,0 +1,19 @@
+package HandlingReq;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserInfoController {
+    @PostMapping("/user")
+    public String userStatus(@RequestBody List<UserInfo> userList) {
+        return String.format("Added %d users", userList.size());
+    }
+
+}
+
+
+
