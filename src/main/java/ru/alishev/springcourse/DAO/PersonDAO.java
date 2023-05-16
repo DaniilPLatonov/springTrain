eef2ffe25c06e3a8d13afa321b928c4951b1b2f6
DaniilPlatonov <d_platonov@internet.ru>
2023-05-16 20:40:10 +0300

    Initial Commit

--- src/main/java/ru/alishev/springcourse/DAO/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/DAO/PersonDAO.java
@@ -6,13 +6,17 @@ import ru.alishev.springcourse.models.Person;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Neil Alishev
+ */
 @Component
 public class PersonDAO {
-    private List<Person> people;
     private static int PEOPLE_COUNT;
+    private List<Person> people;
 
     {
         people = new ArrayList<>();
+
         people.add(new Person(++PEOPLE_COUNT, "Tom"));
         people.add(new Person(++PEOPLE_COUNT, "Bob"));
         people.add(new Person(++PEOPLE_COUNT, "Mike"));
@@ -24,14 +28,21 @@ public class PersonDAO {
     }
 
     public Person show(int id) {
-        return people.stream()
-                .filter(person -> person.getId() == id)
-                .findAny()
-                .orElse(null);
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
 
-    public void save(Person person){
+    public void save(Person person) {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
     }
 }
